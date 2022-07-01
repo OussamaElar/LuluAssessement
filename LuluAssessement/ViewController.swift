519ea1f7f392dc7f0c8fbba4572e6f03ef2678f4
OussamaElar <oussama.elar@gmail.com>
2022-07-01 11:53:54 -0400

    Initial Commit

---
+++ LuluAssessement/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LuluAssessement
+//
+//  Created by Ouss Elar on 7/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
